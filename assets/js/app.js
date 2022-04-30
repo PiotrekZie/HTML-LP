eb952bb07527f2306f9f6c49e92cb28a55882a3f
Pete <zzieluuuu@gmail.com>
2022-04-30 21:48:52 +0200

    Add payment menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -85,10 +85,12 @@ const currentProductImg = document.querySelector(".productImg");
 const currentProductTitle = document.querySelector(".productTitle");
 const currentProductPrice = document.querySelector(".productPrice");
 const currentProductColors = document.querySelectorAll(".color");
-const currentProductSiozes = document.querySelectorAll(".size");
+const currentProductSizes = document.querySelectorAll(".size");
+
 
 menuItems.forEach((item, index) => {
     item.addEventListener("click", () => {
+
       // zmiana obecnego slajdu
       wrapper.style.transform = `translateX(${-100 * index}vw)`;
 
@@ -106,3 +108,32 @@ menuItems.forEach((item, index) => {
       });
     });
   });
+
+  currentProductColors.forEach((color,index)=>{
+    color.addEventListener("click",()=>{
+      currentProductImg.src = choosenProduct.colors[index].img;
+    });
+  });
+
+currentProductSizes.forEach((size,index)=>{
+  size.addEventListener("click", ()=>{
+    currentProductSizes.forEach((size)=> {
+    size.style.backgroundColor = "white"
+    size.style.color = "black"
+    })
+    size.style.backgroundColor = "black"
+    size.style.color = "white"
+  });
+});
+
+const ProductButtom = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+ProductButtom.addEventListener("click", ()=>{
+    payment.style.display="flex"
+  })
+
+close.addEventListener("click", ()=>{
+    payment.style.display="none"
+  })
